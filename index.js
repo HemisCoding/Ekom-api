60f0c168703d736a14fca00fc7badb67cd53a5b6
HemisCoding <plushytowel@gmail.com>
2023-06-13 13:39:15 +0300

    1st

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
+mongoose.connect
+('mongodb+srv://hemiz:BlqkXttqj8jrbl2x@cluster0.r4xakkr.mongodb.net/'
+).then (()=> console.log("DBConnection Successfull!")).catch((err)=>{console.log(err)})
+
+app.get("/api/test", () => {
+    console.log("test is successfull")
+})
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+app.listen(5001, ()=> {
+    console.log("Backend server is running at port: 5001!")
+})
